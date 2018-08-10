b0d8eb26ef3b4c35da30b28a700a0bc8febc1732
Lauro Jacques - DTI <lauro.jacques@dtidigital.com.br>
2018-08-10 17:16:12 -0300

    Mais frases. Mais força.

--- digital-crafter.js
+++ digital-crafter.js
@@ -7,16 +7,26 @@ const TOKEN = fs.readFileSync('beta-token.token', 'utf8');
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 let fraseAleatoria = () => {
-  return 'Espero ter ajudado';
+  const listaDeFrases = [
+    'Espero ter ajudado',
+    'Sempre feliz em contribuir',
+    'Não precisa agradecer',
+    'Ao seu dispor',
+    'Ou não..',
+    'Embora talvez no futuro',
+    ''
+  ]
+  return listaDeFrases[Math.floor(Math.random() * listaDeFrases.length)];
 };
 
 bot.on('message', (msg) => {
 
-  let padraoAlguem = /u|e|é/gi;
+  let padraoRemocao = /u|e|é/gi;
+  let padraoPontuacao = /\?|\.|\!|\,/g;
   let textoSeparado = msg.text.toString().toLowerCase().split(' ');
-  let matchPadraoIndex = msg.text.toString().toLowerCase().replace(padraoAlguem, '').split(' ').indexOf('algm');
+  let matchPadraoIndex = msg.text.toString().toLowerCase().replace(padraoRemocao, '').split(' ').indexOf('algm');
   if (matchPadraoIndex !== -1 && matchPadraoIndex+1 !== textoSeparado.length) {
-    let proximaPalavra = textoSeparado[matchPadraoIndex + 1];
+    let proximaPalavra = textoSeparado[matchPadraoIndex + 1].replace(padraoPontuacao, '');
     bot.sendMessage(msg.chat.id, `Alguém ${proximaPalavra.toLowerCase()}, ${msg.chat.first_name}. ${fraseAleatoria()}.`);
   }
 });
